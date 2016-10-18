823fc5d270476b6acc825ae2ae2a3cd91a6bd27c
ewells1 <ewells1@binghamton.edu>
2016-10-18 12:18:36 -0400

    Outline of LemmaIndexMadred and some code, some details still need to be worked out.

--- BigData_A2/src/main/java/lemma/LemmaIndexMapred.java
+++ BigData_A2/src/main/java/lemma/LemmaIndexMapred.java
@@ -9,9 +9,13 @@ import org.apache.hadoop.mapreduce.Mapper;
 import util.StringIntegerList;
 import edu.umd.cloud9.collection.wikipedia.WikipediaPage;
 
+import Tokenizer;
+
 /**
- * 
- *
+ * Author: Elizabeth
+ * Creates an index of lemma frequencies for each article.
+ * This definitely doesn't work yet but I think it's a good outline.
+ * I just don't know what the offset is for.
  */
 public class LemmaIndexMapred {
 	public static class LemmaIndexMapper extends Mapper<LongWritable, WikipediaPage, Text, StringIntegerList> {
@@ -19,7 +23,16 @@ public class LemmaIndexMapred {
 		@Override
 		public void map(LongWritable offset, WikipediaPage page, Context context) throws IOException,
 				InterruptedException {
-			// TODO: implement Lemma Index mapper here
+			// Get content of page
+			String title = page.getTitle();
+			String text = page.getWikiMarkup();
+			// Tokenize? Or does this go somewhere else?
+			tokenizer = new Tokenizer();
+			lemmas = tokenizer.tokenize(text);
+			// Count lemmas
+			// Do I just go through every lemma manually or is there a better way to do this?
+			// Write output
+			// Not sure how to do this to get the correct format.
 		}
 	}
 }
