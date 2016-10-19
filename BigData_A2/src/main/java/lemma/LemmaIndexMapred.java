f4f227aa39f552ba847d6882bb4d8737db02be84
ewells1 <ewells1@binghamton.edu>
2016-10-18 23:24:41 -0400

    LemmaIndexMapred completed

--- BigData_A2/src/main/java/lemma/LemmaIndexMapred.java
+++ BigData_A2/src/main/java/lemma/LemmaIndexMapred.java
@@ -1,7 +1,6 @@
 package lemma;
 
 import java.io.IOException;
-import java.util.List;
 
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -9,13 +8,14 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import util.StringIntegerList;
 import edu.umd.cloud9.collection.wikipedia.WikipediaPage;
+
+import java.util.HashMap
+
 import lemma.Tokenizer;
 
 /**
  * Author: Elizabeth
  * Creates an index of lemma frequencies for each article.
- * This definitely doesn't work yet but I think it's a good outline.
- * I just don't know what the offset is for.
  */
 public class LemmaIndexMapred {
 	public static class LemmaIndexMapper extends Mapper<LongWritable, WikipediaPage, Text, StringIntegerList> {
@@ -23,16 +23,24 @@ public class LemmaIndexMapred {
 		@Override
 		public void map(LongWritable offset, WikipediaPage page, Context context) throws IOException,
 				InterruptedException {
-			// Get content of page
-			String title = page.getTitle();
+			// Get content of page - comes from WikiPage from GetArticlesMapred
+			Text title = page.getTitle();
 			String text = page.getWikiMarkup();
-			// Tokenize? Or does this go somewhere else?
-			Tokenizer tokenizer = new Tokenizer();
-			List<String> lemmas = tokenizer.tokenize(text);
+			// Tokenize
+			tokenizer = new Tokenizer();
+			lemmas = tokenizer.tokenize(text);
 			// Count lemmas
-			// Do I just go through every lemma manually or is there a better way to do this?
+			Map<String, Integer> freqs = new HashMap<String, Integer>;
+			for (lemma : lemmas){
+				if(freqs.containsKey(lemma)){
+					freqs.put(lemma, freqs.get(lemma) + 1);
+				} else {
+					freqs.put(lemma, 1)
+				}
+			}
+			StringIntegerList list = new StringIntegerList(freqs);
 			// Write output
-			// Not sure how to do this to get the correct format.
+			context.write(title, list);
 		}
 	}
 }
