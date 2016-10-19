6608dc330c35d92dc21b3bc8501165de9ad3a898
ewells1 <ewells1@binghamton.edu>
2016-10-19 13:20:32 -0400

    Fixed errors in LemmaIndexMapred and added main

--- BigData_A2/src/main/java/lemma/LemmaIndexMapred.java
+++ BigData_A2/src/main/java/lemma/LemmaIndexMapred.java
@@ -9,7 +9,8 @@ import org.apache.hadoop.mapreduce.Mapper;
 import util.StringIntegerList;
 import edu.umd.cloud9.collection.wikipedia.WikipediaPage;
 
-import java.util.HashMap
+import java.util.HashMap;
+import java.util.List;
 
 import lemma.Tokenizer;
 
@@ -27,20 +28,45 @@ public class LemmaIndexMapred {
 			Text title = page.getTitle();
 			String text = page.getWikiMarkup();
 			// Tokenize
-			tokenizer = new Tokenizer();
-			lemmas = tokenizer.tokenize(text);
+			Tokenizer tokenizer = new Tokenizer();
+			List lemmas = tokenizer.tokenize(text);
 			// Count lemmas
 			Map<String, Integer> freqs = new HashMap<String, Integer>;
-			for (lemma : lemmas){
-				if(freqs.containsKey(lemma)){
+			for (String lemma : lemmas){
+				if (freqs.containsKey(lemma)) {
 					freqs.put(lemma, freqs.get(lemma) + 1);
 				} else {
-					freqs.put(lemma, 1)
+					freqs.put(lemma, 1);
 				}
 			}
 			StringIntegerList list = new StringIntegerList(freqs);
 			// Write output
 			context.write(title, list);
 		}
+	}
+
+	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException, InterruptedException{
+
+		// Load Class with ClassLoader
+		ClassLoader cl = FindNames.class.getClassLoader();
+
+		// Get args
+		GenericOptionsParser gop = new GenericOptionsParser(conf, args);
+		String[] otherArgs = gop.getRemainingArgs();
+
+		// Get configuration to set up job
+		Configuration conf = new Configuration();
+
+		// Get job and set variables
+		Job job = Job.getInstance(conf, "Lemma Index");
+		job.setJarByClass(LemmaIndexMapred.class);
+		job.setMapperClass(LemmaIndexMapper.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(StringIntegerList.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		// Submit job and wait until it's completed to end program
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }
