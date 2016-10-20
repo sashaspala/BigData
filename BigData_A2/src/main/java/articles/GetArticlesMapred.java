237843332b3a878b53945d4724b15baf07539cf2
schuy <schuy@JARVIS>
2016-10-20 04:42:29 -0400

    Working...ish
    pom updated to make a fat jar with dependency jars included
    people.txt included as a resource because I couldn't get it to work as a
    third argument
    GetArticlesMapred works... on trivially small datasets. On larger wiki
    dumps, it throws a "bytes consumed" error, which, as far as I can tell,
    super shouldn't be happening. It's only supposed to show when working
    with gzipped files (we're not), and from the comments it seems like
    debug code that should have been removed. I think I might be the first
    person to ever actually see this error thrown.
    
    I'm probably just using the wrong version of a library or something.
    Maybe it'll work better on someone else's machine?

--- BigData_A2/src/main/java/articles/GetArticlesMapred.java
+++ BigData_A2/src/main/java/articles/GetArticlesMapred.java
@@ -7,10 +7,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Scanner;
 import java.util.Set;
 import java.util.jar.JarFile;
 
@@ -25,12 +23,9 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import edu.umd.cloud9.collection.wikipedia.WikipediaPage;
-import edu.umd.cloud9.collection.wikipedia.WikipediaPageInputFormat;
+//import edu.umd.cloud9.collection.wikipedia.WikipediaPageInputFormat;
+import util.WikipediaPageInputFormat;
 //import mapreduce.NameCount.Map;
-import mapreduce.NameCount;
-import mapreduce.NameCount.NameFreqsCombiner;
-import mapreduce.NameCount.NameFreqsMapper;
-import mapreduce.NameCount.NameFreqsReducer;
 
 /**
  * This class is used for Section A of assignment 1. You are supposed to
@@ -45,20 +40,20 @@ public class GetArticlesMapred {
 	 * Input:
 	 * 		Page offset 	WikipediaPage
 	 * Output
-	 * 		Page offset 	WikipediaPage
+	 * 		Text (name)		Text (xml page)
 	 * @author Tuan
 	 *
 	 */
 	//@formatter:on
-	public static class GetArticlesMapper extends Mapper<LongWritable, WikipediaPage, LongWritable, WikipediaPage> {
+	public static class GetArticlesMapper extends Mapper<LongWritable, WikipediaPage, Text, Text> {
 		public static Set<String> peopleArticlesTitles = new HashSet<String>();
 		private final static IntWritable one = new IntWritable(1);	//is this just here out of habit?
-		private Text fullName = new Text();
-		//private HashMap<String, ArrayList<String>> names;
-		private ArrayList<String> names;
+		private Text fullName = new Text();							//don't know what this is either
+		//private HashMap<String, ArrayList<String>> names;			//why?
+		private HashSet<String> names;
 
 		@Override
-		protected void setup(Mapper<LongWritable, WikipediaPage, LongWritable, WikipediaPage>.Context context)
+		protected void setup(Mapper<LongWritable, WikipediaPage, Text, Text>.Context context)
 				throws IOException, InterruptedException {
 //		protected void setup(Context context) throws IOException, InterruptedException {
 			// TODO: You should implement people articles load from
@@ -69,43 +64,27 @@ public class GetArticlesMapred {
 			super.setup(context);
 			
 			URI[] files = context.getCacheFiles();
-			File wikiFile = new File(files[0].getPath());	
-//			Scanner scan = new Scanner(wikiFile);
 			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(files[0].getPath()), "UTF-8"));
-			//names = new HashMap<String, ArrayList<String>>(1000000);	//expecting ~700k keys, 0.75 default load factor
-			names = new ArrayList<String>();
+			names = new HashSet<String>();
 			
 			String line = reader.readLine();
 			while(line != null){
-				//names.put(line, new ArrayList<String>());	//there are about 60k repeats but who cares
-				names.add(line);	//it's unimportant that there are some repeats
+				names.add(line);
 				line = reader.readLine();				
 			}
 			reader.close();
-			System.out.println("exiting setup");
 		}
 
 		@Override
 		public void map(LongWritable offset, WikipediaPage inputPage, Context context)
 				throws IOException, InterruptedException {
 			// TODO: You should implement getting article mapper here
-			System.out.println("entering map");
 			
-//			if(names.containsKey(inputPage.getTitle())){
-//				System.out.println("found a name:" + inputPage.getTitle());
-//				ArrayList<String> articles = names.get(inputPage.getTitle());
-//				articles.add(inputPage.getContent());
-//				names.put(inputPage.getTitle(), articles);
-//			}
-//			
-//			for(String name : names.keySet()){
-//				System.out.println("writing some stuff");
-//				ArrayList<String> articles = names.get(name);
-//				for(int i=0; i<articles.size(); i++)
-//					context.write(new Text(name), new Text(articles.get(i)));
-//			}
-			
-			System.out.println("exiting map");
+			String title = inputPage.getTitle();
+			String xml = inputPage.getRawXML();
+			if(names.contains(title)){
+				context.write(new Text(title), new Text(xml));
+			}
 		}
 	}
 
@@ -116,15 +95,15 @@ public class GetArticlesMapred {
 		Job job = Job.getInstance(conf, "get articles");
 		
 //		job.addCacheFile(new Path(args[2]).toUri());	//people.txt is 3rd argument
-		//couldn't get the above to work "URI is not absolute" or some bs
-		ClassLoader cl = NameCount.class.getClassLoader();
-		job.addCacheFile(cl.getResource("people.txt").toURI());
+		//couldn't get the above to work: "URI is not absolute" or some bs
+		ClassLoader cl = GetArticlesMapred.class.getClassLoader();
+		job.addCacheFile(cl.getResource("people.txt").toURI());		//just going to include this as a resource like in assignment 1
 		
 		job.setJarByClass(GetArticlesMapred.class);
 		job.setMapperClass(GetArticlesMapper.class);
-		//job.setInputFormatClass(WikipediaPageInputFormat.class);	//why doesn't this work?
-		job.setMapOutputKeyClass(LongWritable.class);
-		job.setMapOutputValueClass(WikipediaPage.class);
+		job.setInputFormatClass(WikipediaPageInputFormat.class);	//why didn't this work? -- because I hadn't imported it =P
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(Text.class);
 		//job.setCombinerClass(GetArticlesCombiner.class);
 		//job.setReducerClass(GetArticlesReducer.class);
 		//job.setOutputKeyClass(Text.class);
