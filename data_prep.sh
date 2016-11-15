e9a91727f7c70076eb1fbd777418fe2c315cda20
Sasha Spala <spala@usc.edu>
2016-11-14 21:16:46 -0500

    added testing command

--- data_prep.sh
+++ data_prep.sh
@@ -8,3 +8,6 @@
 #use respective training/testing locs for meghan's classifier bit
 ##training outputs a model based on the training data
 ##testing uses that model to test on the test data
+
+./mahout trainnb -i ${PATH_TO_TFIDF_VECTORS} -o ${PATH_TO_MODEL}/model -ow
+#where {Path_to_model} is where we want to store our model for later use
