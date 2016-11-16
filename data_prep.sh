76a5bde40e3517f39596efd9fc93c283b43a011b
Sasha Spala <spala@usc.edu>
2016-11-15 22:01:49 -0500

    added run.sh with edits

--- data_prep.sh
+++ data_prep.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+inputloc = $1
 
-./mahout seqdirectory -i <path to shortened lemma index folder> -o <path to desired output location>  #make sure lemma index is the only thing in this directory
-./mahout seq2sparse -i <output location from above command> -o <path to desired output location> -lnorm -wt tfidf
-./mahout split -i <output location from seq2sparse command> --trainingOutput <desired output location for training data> --testOutput <desired loc for test data> --randomSelectionPct 40 --overwrite --sequenceFiles -xm sequential
+mkdir data
+mkdir data/seqdir
+mkdir data/sparse
+mkdir model
+mkdir model/model
+mkdir model/labelindex
+
+./mahout seqdirectory -i $inputloc -o data/seqdir #make sure lemma index is the only thing in this directory
+./mahout seq2sparse -i data/seqdir -o data/sparse -lnorm -wt tfidf
 
 #use respective training/testing locs for meghan's classifier bit
 ##training outputs a model based on the training data
 ##testing uses that model to test on the test data
 
-./mahout trainnb -i ${PATH_TO_TFIDF_VECTORS} -o ${PATH_TO_MODEL}/model -li ${PATH_TO_MODEL}/labelindex -ow
+./mahout trainnb -i data/sparse -o model/model -li model/labelindex -ow
 #where {Path_to_model} is where we want to store our model for later use
