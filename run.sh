112af5f8c348239d726ecfb4301586bcbf08bd3d
ewells1 <ewells1@binghamton.edu>
2016-11-15 22:48:56 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,16 +5,16 @@ training_index_loc = $1
 testing_index_loc = $2
 
 #run first map reduce job to classify training data
-uber-BigData_A2-0.0.1-SNAPSHOT.jar professions.ProfessionIndexMapred -Dmapred.job.queue.name=hadoop02 $index_loc output/data
+yarn jar uber-BigData_A2-0.0.1-SNAPSHOT.jar professions.ProfessionIndexMapred -Dmapred.job.queue.name=hadoop02 $index_loc output/data
 
 #call data_prep on first mr job
 ./data_prep.sh output/data
 
 #now run naive bayes
-uber-BigData_A2-0.0.1-SNAPSHOT.jar professions.NaiveBayes -Dmapred.job.queue.name=hadoop02 model/model $testing_index_loc nboutput/data model/labelindex
+yarn jar uber-BigData_A2-0.0.1-SNAPSHOT.jar professions.NaiveBayes -Dmapred.job.queue.name=hadoop02 model/model $testing_index_loc nboutput/data model/labelindex
 
 #finally, run evaluation map reduce job
-uber-BigData_A2-0.0.1-SNAPSHOT.jar professions.ProfessionIndexMapred -Dmapred.job.queue.name=hadoop02 nboutput evaluate/mr
+yarn jar uber-BigData_A2-0.0.1-SNAPSHOT.jar professions.ProfessionIndexMapred -Dmapred.job.queue.name=hadoop02 nboutput evaluate/mr
 
 #and return evaluation post-processing
 java ProcessEvaluation evaluate/mr
